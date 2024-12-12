654c8f5acd7db92f1d623406d82a8921fca27b0e
trancohung2001 <trancohung2016@gmail.com>
2024-12-12 19:53:53 +0700

    game

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
 const items = document.querySelectorAll(".item");
 const rsButton = document.querySelector(".reset-btn");
+const startButton = document.querySelector(".start-btn");
+const results = document.querySelectorAll(".results .result img");
+let playerChoices = [];
+const maxBets = 3;
+const images = ["bau", "ca", "cua", "ga", "huou", "tom"];
+let totalBets = 0;
 
 items.forEach((item) => {
-    const counter = item.querySelector("p");
-    const img = item.querySelector(".item img");
+    item.addEventListener("click", () => {
+        const counter = item.querySelector("p");
+        const currentCount = parseInt(counter.textContent);
+        if (totalBets >= maxBets) return;
 
-    img.addEventListener("click", () => {
-        let currentValue = parseInt(counter.textContent);
-        counter.textContent = currentValue + 1;
-        if (currentValue > 3) {
-            items.style.borderColor = "red";
+        if (totalBets < maxBets) {
+            counter.textContent = currentCount + 1;
+            totalBets++;
+            playerChoices.push(item.getAttribute('data-name'));
+            console.log(playerChoices);
+        }
+
+        if (totalBets >= maxBets) {
+            items.forEach((item) => {
+                item.style.border = "2px solid red";
+            })
         }
     })
 })
 
+startButton.addEventListener("click", () => {
+    let count = 0;
+
+    const interval = setInterval(() => {
+        results.forEach((result) => {
+            const randomImg = Math.floor(Math.random() * images.length);
+            result.setAttribute("src", `./Assets/${images[randomImg]}.png`);
+        });
+
+        count++;
+        if (count >= 20) {
+            clearInterval(interval);
+
+            const randomResults = [];
+            for (let i = 0; i < 3; i++) {
+                const randomIndex = Math.floor(Math.random() * images.length);
+                randomResults.push(images[randomIndex]);
+            };
+
+            results.forEach((result, index) => {
+                result.setAttribute("src", `./Assets/${randomResults[index]}.png`);
+            });
+
+            console.log(playerChoices.values());
+            // console.log(randomResults.every(choice => playerChoices.includes(choice)));
+        }
+    }, 150)
+    
+})
+
 rsButton.addEventListener("click", () => {
+    totalBets = 0;
+    playerChoices = [];
     items.forEach((item) => {
-        const counter = item.querySelector("p");
-        counter.textContent = 0;
+        item.querySelector("p").textContent = 0;
+        item.style.border = "1px solid black";
     })
 })
